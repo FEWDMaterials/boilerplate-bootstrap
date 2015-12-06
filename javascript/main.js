8e4d734956ea196db26aa9744340c85671ce701d
Steve <screswick@gmail.com>
2015-12-06 17:14:37 -0500

    added class notes

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,31 @@
 /* CURRENTLY IN: javascript/main.js */
+
+$('.js-slick').slick({
+  arrows: false
+});
+
+$('.js-prev').on('click', slickPrev );
+$('.js-next').on('click', slickNext );
+
+function slickPrev() {
+  $('.js-slick').slick( 'slickPrev' );
+}
+
+function slickNext() {
+  $('.js-slick').slick( 'slicklickNext' );
+}
+
+$('.js-page-1').on('click', slickGoTo);
+$('.js-page-2').on('click', slickGoTo);
+$('.js-page-3').on('click', slickGoTo);
+
+function slickGoTo() {
+  var me = $( this );
+
+  var imageSlideNumber = me.attr('data-id');
+
+  $('.js-slick').slick('slickGoTo', imageSlideNumber);
+
+  $('.page-active').removeClass('page-active');
+  me.addClass('page-active');
+}
